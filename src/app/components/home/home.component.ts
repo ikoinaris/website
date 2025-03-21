c26451bd1d1dd0eb77e7a68c652e0139b64c5cb2
ikoinaris <i.koinaris@gmail.com>
2025-03-21 22:25:09 +0200

    social media configured

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  socialMedia = [
+    { name: 'Mail', icon: 'assets/contact/mail.png', link:'mailto: i.koinaris@gmail.com' },
+    { name: 'LinkedIn', icon: 'assets/contact/linkedin.png', link: 'https://www.linkedin.com/in/ikoinaris/' },
+    { name: 'GitHub', icon: 'assets/contact/git.png', link: 'https://github.com/ikoinaris' }
+  ];
+
   currentText: string = '';
   textIndex: number = 0;
   charIndex: number = 0;
