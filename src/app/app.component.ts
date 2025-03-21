242c3893616cd2b6bcdd3a9350dcc7397de550c8
ikoinaris <i.koinaris@gmail.com>
2025-03-21 22:06:50 +0200

    sidebar for contact implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  socialMedia = [
+    { name: 'Mail', icon: 'assets/contact/mail.png', link:'mailto: i.koinaris@gmail.com' },
+    { name: 'LinkedIn', icon: 'assets/contact/linkedin.png', link: 'https://www.linkedin.com/in/ikoinaris/' },
+    /*{ name: 'X', icon: 'assets/contact/x.png', link: 'https://x.com/ikoinaris' },*/
+    { name: 'GitHub', icon: 'assets/contact/git.png', link: 'https://github.com/ikoinaris' }
+  ];
+  
   title = 'ioannis-koinaris-software-engineer';
-
   menuOpen = false;
+  sidebarExpanded = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  expandSidebar() {
+    this.sidebarExpanded = true;
+  }
+
+  collapseSidebar() {
+    this.sidebarExpanded = false;
   }
 }
