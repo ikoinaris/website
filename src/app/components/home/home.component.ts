d7a0a2d5cf7cb75ee8a79aacbe0d723df979efc6
ikoinaris <i.koinaris@gmail.com>
2025-03-23 21:04:40 +0200

    contact method updated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit {
   socialMedia = [
     { name: 'Mail', icon: 'assets/contact/mail.png', link:'mailto: i.koinaris@gmail.com' },
     { name: 'LinkedIn', icon: 'assets/contact/linkedin.png', link: 'https://www.linkedin.com/in/ikoinaris/' },
+    { name: 'X', icon: 'assets/contact/x.png', link: 'https://x.com/ikoinaris' },
     { name: 'GitHub', icon: 'assets/contact/git.png', link: 'https://github.com/ikoinaris' }
   ];
 
