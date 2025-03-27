eb66a243c379e23f468b9532345aca46d81e6745
ikoinaris <i.koinaris@gmail.com>
2025-03-27 23:08:22 +0200

    mail icon

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   socialMedia = [
-    { name: 'Mail', icon: 'assets/contact/mail.png', link:'mailto: i.koinaris@gmail.com' },
     { name: 'LinkedIn', icon: 'assets/contact/linkedin.png', link: 'https://www.linkedin.com/in/ikoinaris/' },
-    { name: 'X', icon: 'assets/contact/x.png', link: 'https://x.com/ikoinaris' },
-    { name: 'GitHub', icon: 'assets/contact/git.png', link: 'https://github.com/ikoinaris' }
+    { name: 'Mail', icon: 'assets/contact/email.png', link:'mailto: i.koinaris@gmail.com' },
+    { name: 'GitHub', icon: 'assets/contact/git.png', link: 'https://github.com/ikoinaris' },
   ];
 
   currentText: string = '';
