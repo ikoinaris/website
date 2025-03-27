726ccd9b09f33117dae6f1740caf62fe564d2e13
ikoinaris <i.koinaris@gmail.com>
2025-03-27 22:53:24 +0200

    skills update

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,55 +7,48 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
-  skillCategories = [
-    { name: 'Programming Languages', skills: [
+  expertiseAreas = [
+    { 
+      name: 'RESTful API Development', 
+      skills: [
         { name: 'Java', icon: 'assets/skills/languages/java.png' },
-        { name: 'Typescript', icon: 'assets/skills/languages/typescript.png' },
-        { name: 'JavaScript', icon: 'assets/skills/languages/javascript.png' },
-        { name: 'HTML', icon: 'assets/skills/languages/html.png' },
-        { name: 'CSS', icon: 'assets/skills/languages/css.png' },
-        { name: 'Python', icon: 'assets/skills/languages/python.png' }
-      ]
-    },
-    { name: 'Frameworks', skills: [
         { name: 'Spring', icon: 'assets/skills/frameworks/spring.png' },
         { name: 'Quarkus', icon: 'assets/skills/frameworks/quarkus.png' },
-        { name: 'Angular', icon: 'assets/skills/frameworks/angular.png' },
-        { name: 'React', icon: 'assets/skills/frameworks/react.png' },
-        { name: 'Node', icon: 'assets/skills/frameworks/node.png' }
+        { name: 'Node', icon: 'assets/skills/frameworks/node.png' },
       ]
     },
-    { name: 'Software', skills: [
-        { name: 'IntelliJ', icon: 'assets/skills/software/intellij.png' },
-        { name: 'VS Code', icon: 'assets/skills/software/vscode.png' },
-        { name: 'Eclipse', icon: 'assets/skills/software/eclipse.png' },
+    { 
+      name: 'Website Development', 
+      skills: [
+        { name: 'HTML', icon: 'assets/skills/languages/html.png' },
+        { name: 'CSS', icon: 'assets/skills/languages/css.png' },
+        { name: 'TypeScript', icon: 'assets/skills/languages/typescript.png' },
+        { name: 'Angular', icon: 'assets/skills/frameworks/angular.png' }
+      ]
+    },
+    { 
+      name: 'CI/CD Orchestration', 
+      skills: [
         { name: 'Git', icon: 'assets/skills/software/github.png' },
         { name: 'Docker', icon: 'assets/skills/software/docker.png' },
-        { name: 'Jenkins', icon: 'assets/skills/software/jenkins.png' },
-        { name: 'Postman', icon: 'assets/skills/software/postman.png' },
-        { name: 'Bruno', icon: 'assets/skills/software/bruno.png' }
-      ] },
-    { name: 'Databases', skills: [
-        { name: 'PostgreSQL', icon: 'assets/skills/databases/postgres.png' },
-        { name: 'MySQL', icon: 'assets/skills/databases/mysql.png' },
-        { name: 'Cosmos DB', icon: 'assets/skills/databases/cosmosdb.png' },
-        { name: 'MongoDB', icon: 'assets/skills/databases/mongodb.png' }
+        { name: 'Jenkins', icon: 'assets/skills/software/jenkins.png' }
       ]
     },
-    { name: 'Cloud Providers', skills: [
-      { name: 'AWS', icon: 'assets/skills/cloud/aws.png' },
-      { name: 'Azure', icon: 'assets/skills/cloud/azure.png' }
-    ]
-  }
+    { 
+      name: 'Cloud Services Management', 
+      skills: [
+        { name: 'AWS', icon: 'assets/skills/cloud/aws.png' },
+        { name: 'Azure', icon: 'assets/skills/cloud/azure.png' }
+      ]
+    },
+    { 
+      name: 'Database Management', 
+      skills: [
+        { name: 'PostgreSQL', icon: 'assets/skills/databases/postgres.png' },
+        { name: 'MySQL', icon: 'assets/skills/databases/mysql.png' },
+        { name: 'CosmosDb', icon: 'assets/skills/databases/cosmosdb.png' },
+        { name: 'MongoDB', icon: 'assets/skills/databases/mongodb.png' }
+      ]
+    }
   ];
-
-  currentSlideIndex = 0;
-
-  nextSlide() {
-    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.skillCategories.length;
-  }
-
-  previousSlide() {
-    this.currentSlideIndex = (this.currentSlideIndex - 1 + this.skillCategories.length) % this.skillCategories.length;
-  }
 }
